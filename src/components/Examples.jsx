6bf96f92f07b8c0d6b395b419beeffe8b3bf5a17
ChinthakaICIEOS <chinthaka@icieos.com>
2025-07-01 16:07:56 +0530

    Forwarding Props To Wrapped Elements

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -28,29 +28,29 @@ export default function Examples() {
     }
 
     return (
-        <Section title="Examples" id="examples">
+        <Section title="Examples" id="examples" className="">
             <menu>
                 <TabButton
                 isSelected={selectedTopic === "components"}
-                onSelect={() => handleSelect("components")}
+                onClick={() => handleSelect("components")}
                 >
                 Components
                 </TabButton>
                 <TabButton
                 isSelected={selectedTopic === "jsx"}
-                onSelect={() => handleSelect("jsx")}
+                onClick={() => handleSelect("jsx")}
                 >
                 JSX
                 </TabButton>
                 <TabButton
                 isSelected={selectedTopic === "props"}
-                onSelect={() => handleSelect("props")}
+                onClick={() => handleSelect("props")}
                 >
                 Props
                 </TabButton>
                 <TabButton
                 isSelected={selectedTopic === "state"}
-                onSelect={() => handleSelect("state")}
+                onClick={() => handleSelect("state")}
                 >
                 State
                 </TabButton>
